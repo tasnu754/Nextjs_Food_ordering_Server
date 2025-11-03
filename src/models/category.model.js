73e51add9554361e04c27dbcca72a63633e69de4
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-11-03 18:41:11 +0600

    Category Model created

---
+++ src/models/category.model.js
@@ -0,0 +1,35 @@
+// models/Category.js
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Generate slug before saving
+categorySchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.slug = this.name.toLowerCase().replace(/[^a-z0-9]+/g, "-");
+  }
+  next();
+});
+
+module.exports = mongoose.model("Category", categorySchema);
